7d499512075ce6deeac7e39f3acc9ce76e31fe15
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-01-31 10:31:51 -0200

    Acréscimos na classe Controladora

--- Agendamento/Controladora.cs
+++ Agendamento/Controladora.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Agendamento.Interfaces;
 
 namespace Agendamento
 {
     public class Controladora
     {
+        private string nome;
         private int qtdeSolicitacoes = 0;
+        private IController controladora;
+
+        public Controladora(string vNome)
+        {
+            this.nome = vNome;
+        }
+
+        public string getNome()
+        {
+            this.qtdeSolicitacoes++;
+
+            return this.nome;
+        }
+
     }
 }
