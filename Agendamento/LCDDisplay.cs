9768c7e55df365908f2f15f1d2b5553dab737fc1
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-01-31 11:34:17 -0200

    Criação de classes que implementam IDisplay (LCDDisplay e PTextbox)

---
+++ Agendamento/LCDDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agendamento.Interfaces;
+
+namespace Agendamento
+{
+    public class LCDDisplay: IDisplay
+    {
+        string valorAtual;
+
+        public void receberDados(string dados)
+        {
+            this.valorAtual = dados;
+        }
+    }
+}
---
+++ Agendamento/PTextbox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agendamento.Interfaces;
+
+namespace Agendamento
+{
+    public class PTextbox : IDisplay
+    {
+        private string leituraAtual;
+
+        public void receberDados(string dados)
+        {
+            this.leituraAtual = dados;
+        }
+    }
+}
